58138ca4d5c27d5046b0310a90cb3d2bff5c24e3
ELehotzky <Lehotzky.Erica@gmail.com>
2018-11-19 16:15:02 -0600

    added Car class

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Car {
+
+	public static void main(String[] args) {
+		int avgMilesPerGallon;
+		String licensePlate;
+		Color paintColor;
+		boolean tailLightsWorking;
+		
+
+	}
+
+}
