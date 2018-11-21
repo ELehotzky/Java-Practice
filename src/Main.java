ccd9b66f87a50ed630a963b64af50f0f3b79da67
ELehotzky <Lehotzky.Erica@gmail.com>
2018-11-20 18:02:26 -0600

    Java array practice

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
 import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-				
-		// TODO Auto-generated method stub
-		String userInput = sc.next();
-		String uppercased = userInput.toUpperCase();
-		System.out.println(userInput);
-		System.out.println(uppercased);
+		// 31, 45, 22, 98, 10
+		int[] numbers = new int[5];
 		
-		char firstChar = userInput.charAt(0);
-		System.out.println(firstChar);
+		numbers[0] = 31;
+		numbers[1] = 45;
+		numbers[2] = 22;
+		numbers[3] = 98;
+		numbers[4] = 10;
 		
-		System.out.println("Contains: " + userInput.contains("Enter".toLowerCase()));
+		int[] numbers2 = {31, 45, 22, 98, 10};
+		
+		Arrays.sort(numbers);
+		System.out.println(Arrays.toString(numbers));
+		
+		String[] myFavoriteCandyBars = {"Twix", "Hershey's", "Kitkat"};
+		System.out.println("Index 1: " + myFavoriteCandyBars[1]);
+		myFavoriteCandyBars[2] = "Butterfinger";
+		System.out.println(myFavoriteCandyBars.length);
+		
+		System.out.println(Array.get(myFavoriteCandyBars, 2));
+		
 	}
 }
